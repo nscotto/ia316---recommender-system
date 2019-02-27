49c0e5fbd902d69d24d039e69a62b8a5a72e3e15
vcharvet <valentin.charvet@telecom-paristech.fr>
2019-02-27 22:02:22 +0100

    [env2] minor fix on model

--- env2/main.py
+++ env2/main.py
@@ -10,18 +10,24 @@ import numpy as np
 
 
 def main():
-    # parser = argparse.ArgumentParser(description='main for second env')
-    # parser.add_argument('--model', dest='model', type=str,
-    #                     help='Model to use (deep or FM)')
-    # args = parser.parse_args()
-    # model_name = args.model
+    parser = argparse.ArgumentParser(description='main for second env')
+    parser.add_argument('--embed-size', dest='n_embed', type=int,
+                        help='size of the embeddings layers')
+    parser.add_argument('--hidden-size', dest='n_hidden', type=int,
+                        help='size of the hidden layers')
+    args = parser.parse_args()
+    embedding_size = args.n_embed
+    hidden_size  = args.n_hidden
     user_id = 'MC15CHD1JU24INEKKPU4'
     adress = 'http://35.180.254.42'
 
     feature_gen = FeatureGenerator(user_id)
     feature_gen.reset()
+    print('Number of users: {}\nNumber of items {}'.format(feature_gen.nb_users,
+                                                           feature_gen.nb_items))
 
-    model = DeepFactorization(embedding_size=64,
+    model = DeepFactorization(embedding_size=embedding_size,
+                              n_hidden=hidden_size,
                               n_user=feature_gen.nb_users,
                               n_item=feature_gen.nb_items,
                               batch_size=64)
@@ -38,6 +44,8 @@ def main():
     mse, mae = 0, 0
     i = 0
 
+    n_iter=1000
+
     while True:
         sleep(0.05)
         i += 1
@@ -47,19 +55,22 @@ def main():
         except requests.exceptions.RequestException as e:
             print('The whole base has been parsed')
             break
+        # if i >= n_iter:
+        #     break
 
         data = req.json()
 
         true_rating = data['rating']
         input_retrain = [np.array([x]) for x in [next_u, next_i, next_var]]
-        model.train(input_retrain, np.array([true_rating]))
+        model.train(input_retrain, np.array([true_rating]), 1, 0)
 
         next_u, next_i, next_var = data['next_user'], data['next_item'], \
             data['next_variables']
 
         mse += (pred - true_rating) ** 2
         mae += abs(pred - true_rating)
-        print('Iteration {}, SE={:.3f}'.format(i, (pred - true_rating)**2))
+        if (i % 10) == 0:
+            print('Iteration {}, SE={:.3f}'.format(i, (pred - true_rating)**2))
 
         input_pred = [np.array([x]) for x in [next_u, next_i, next_var]]
         pred = model.predict(input_pred)[0][0]
